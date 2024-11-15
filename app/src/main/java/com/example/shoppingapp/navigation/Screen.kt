2ec07d77b9d6e9287804cd3b4546b8463276be4b
Yellow Bear <percussionpeng@gmail.com>
2024-11-14 19:21:26 -0800

    update tab Screen.kt

--- app/src/main/java/com/example/shoppingapp/navigation/Screen.kt
+++ app/src/main/java/com/example/shoppingapp/navigation/Screen.kt
@@ -2,14 +2,18 @@ package com.example.shoppingapp.navigation
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.shoppingapp.R
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
-//    object Home : Screen("home", R.string.home, Icons.Default.Home)
-//    object Search : Screen("search", R.string.search, Icons.Default.Search)
-//    object Profile : Screen("profile", R.string.profile, Icons.Default.Person)
+    object Home : Screen("home", R.string.home, Icons.Default.Home)
+    object Shop : Screen("shop", R.string.shop, Icons.Default.Search)
+    object Favorites : Screen("favorites", R.string.favorites, Icons.Default.Favorite)
+    object Bag : Screen("bag", R.string.bag, Icons.Default.ShoppingCart)
+    object Account : Screen("account", R.string.account, Icons.Default.Person)
 }
